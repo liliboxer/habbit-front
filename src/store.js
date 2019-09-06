030da129a8da37d0d093cb6b3b68ad72363955cf
Lili Boxer <ninaliboxer@gmail.com>
2019-09-06 11:11:58 -0700

    redux store set up

--- src/store.js
+++ src/store.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { promiseMiddleware } from 'promise-middleware-redux';
+import reducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
-  
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer,
+  composeEnhancers(
+    applyMiddleware(
+      promiseMiddleware
+    )
+  )
 );
